5314fba268a9bddca9a64d656827718a64e83e38
pankeeva <pankeeva@gmail.com>
2019-12-22 14:04:11 +0200

    my tasks

---
+++ pankeyevasd/first/task1.py
@@ -0,0 +1,6 @@
+# Користувач вводить текст. Вивести усі слова цього тексту,що містить велику букву, у вигляді списку.
+print ("enter text ")
+t = input()
+ word =  s.split ( )
+ for  letter in word.lower( ):
+     print (word )
---
+++ pankeyevasd/first/task3.py
@@ -0,0 +1,4 @@
+# Для заданого списку чисел, вивести суму усіх парних та непарних елементів цього списку.
+n= input('enter element {}':.format(i=1))
+ ist.append(n)
+print( sum(x is None for x in ist))
